572bacc853ec36d0aa703162ce2da73e3e60582e
Muhammadyusuf Ibrohimov <mrimofficial7@gmail.com>
2025-12-15 16:58:35 +0500

    Updated homepage design, build was successful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useEffect, useRef, useState } from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
