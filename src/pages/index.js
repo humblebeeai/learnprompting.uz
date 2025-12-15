885111d4769d4ad2b12c10f4dd9b2d1feaa0a9bd
Muhammadyusuf Ibrohimov <mrimofficial7@gmail.com>
2025-12-15 16:27:31 +0500

    Remove unused imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
